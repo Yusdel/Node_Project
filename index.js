8c07f35f9bfa0f2ffb85dbb0ed057990a29a2f10
Neko ^_^ <antocapa98@gmail.com>
2019-12-18 21:46:05 +0100

    Add login and auto_register option on index

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const http = require('http').createServer(app);
 const child_process = require('child_process');
 const config = require(__MainDir + '/config.json');
 const Port = (new URL(config.MainHost)).port;
+const crypto = require('crypto');
+const fs = require('fs');
 
 const Games = config.Games
 
@@ -17,6 +19,24 @@ StartProcess = (path, timeToRestart) => {
             setTimeout(() => StartProcess(child.spawnargs[1], timeToRestart), timeToRestart)
     })
 
+}
+
+const secret = 'piero va al mercato';
+HashCode = function(password){
+    const hash = crypto.createHmac('sha256', secret)
+        .update(password)
+        .digest('hex');
+                  
+    return hash;
+}
+
+InsertUser = (username, password) => {
+    const Users = require(__MainDir + '/users.json');
+    Users.push({
+        Name: username, 
+        Password: HashCode(password)
+    })
+    fs.writeFileSync(process.cwd() + '/users.json', JSON.stringify(Users) , 'utf-8');
 }
 
 Games.forEach(game => StartProcess(game.Path, 2000))
@@ -33,15 +53,35 @@ app.get('/Info', function(req, res){
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-    let infos = []
-    Games.forEach((x) => {
-        let info = {}
-        info.Name = x.Name
-        info.Host = x.Host
-        infos.push(info)
-    })
-    res.send(infos);
+    for (i in config.Games) {
+        dati.push({Nome: config.Games[i].Name ,Host: config.Games[i].Host}) 
+    }
+         
+    res.send(dati);
 });
+
+app.get('/Login/:user/:password', function(req, res){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+    const Users = require(__MainDir + '/users.json');
+    let find = Users.find(x => x.Name == req.params.user);
+    if (!find){
+        if(config.Enable_Auto_Registration){
+            InsertUser(req.params.user, req.params.password);
+            res.sendStatus(200);
+            return;
+        }
+        res.sendStatus(401)
+        return
+    }
+    if (find.Password != HashCode(req.params.password)){
+        res.sendStatus(401)
+        return
+    }
+    res.sendStatus(200);
+});
+
 
 http.listen(Port, function(){
     console.log('Main port:', http.address().port);
